71b29880a6ca0e806fb6c5092d2800eab9dabb1e
DoanVanDanh <danhcntt15@gmail.com>
2018-02-01 19:44:10 +0700

    first commit

---
+++ Spring_Basic_2/src/A.java
@@ -0,0 +1,11 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericApplicationContext;
+
+public class A {
+	public static void main(String[] args) {
+		ApplicationContext applicationContext = new GenericApplicationContext();
+		if(applicationContext.containsBean("a"))
+			System.out.println("aaaaaa");
+		aaa;
+	}
+}
